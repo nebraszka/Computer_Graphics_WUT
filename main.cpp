a7f3905a706f32976e6cc7cf83650f58a49a924f
nebraszka <majusxp@gmail.com>
2023-04-10 16:32:33 +0200

    Add vertex and frame shader

--- main.cpp
+++ main.cpp
@@ -9,23 +9,37 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 #define WINDOW_WIDTH 800 
 #define WINDOW_HEIGHT 600
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                    "out vec4 FragColor;\n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                    "}\n\0";
 
 int main()
 {
     /*
-        Note:
-        GLFW is a library, written in C, specifically targeted at OpenGL.
-        GLFW gives us the bare necessities required for rendering goodies to the screen.
-        It allows us to create an OpenGL context, define window parameters, 
-        and handle user input, which is plenty enough for our purposes. 
-
-        Source: https://learnopengl.com/Getting-started/Creating-a-window
-    */
+     *  Note:
+     *  GLFW is a library, written in C, specifically targeted at OpenGL.
+     *  GLFW gives us the bare necessities required for rendering goodies to the screen.
+     *  It allows us to create an OpenGL context, define window parameters, 
+     *  and handle user input, which is plenty enough for our purposes. 
+     * 
+     *  Source: https://learnopengl.com/Getting-started/Creating-a-window
+     */
 
     // Configuration of GLFW
     glfwInit();
@@ -51,6 +65,71 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     } 
+
+    // --------------------------- Vertex shader ------------------------------
+    // Vertex shader program - compile
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Vertex shader compile errors
+    int success;
+    char infoLog[512];
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // --------------------------- Fragment shader ------------------------------
+    // Fragment shader program - compile
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Fragment shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // --------------------------- Linking shaders ------------------------------
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Shader program errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        ...
+    }
+
+    glUseProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Loading the vertices into vertex buffer objects (VBO)
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+    unsigned int VBO;
+    glGenBuffers(1, &VBO); 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /*
+     *  Note:
+     *  Modern OpenGL requires that we at least set up a vertex and fragment shader if we want to do some rendering
+     *  Source: https://learnopengl.com/Getting-started/Hello-Triangle
+     */
 
     // On window resize
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
